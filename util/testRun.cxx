3e79f61c0d4684cf2da4e6d04a926ccb3a007264
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-18 14:13:39 +0100

    fixes

--- util/testRun.cxx
+++ util/testRun.cxx
@@ -32,37 +32,37 @@ int main( int argc, char* argv[] ) {
    //SH::scanDir (sh, list, "DAOD_EXOT9.04655450._000002.pool.root.1");
    //SH::scanDir (sh, list, "AOD.01494882._113691.pool.root.1"); // specifying one particular file for testing
 
-   // Set the name of the input TTree. It's always "CollectionTree"
-   // for xAOD files.
+   /// Set the name of the input TTree. It's always "CollectionTree"
+   /// for xAOD files.
    sh.setMetaString( "nc_tree", "CollectionTree" );
 
-   // Print what we found:
+   /// Print what we found:
    sh.print();
 
-   // Create an EventLoop job:
+   /// Create an EventLoop job:
    EL::Job job;
    job.sampleHandler( sh );
 
-   // Specify that we only want to run on 1k events
+   /// Specify that we only want to run on 1k events
    if (nEvent!=-1)
 	   job.options()->setDouble(EL::Job::optMaxEvents, nEvent);
 
-   // define an output and an ntuple associated to that output 
-   EL::OutputStream output  ("myOutput");
-   job.outputAdd (output);
-   EL::NTupleSvc *ntuple = new EL::NTupleSvc ("myOutput");
-   job.algsAdd (ntuple);
+   /// define an output and an ntuple associated to that output 
+//    EL::OutputStream output  ("myOutput");
+//    job.outputAdd (output);
+//    EL::NTupleSvc *ntuple = new EL::NTupleSvc ("myOutput");
+//    job.algsAdd (ntuple);
 
    /// Add our analysis to the job:
    MyxAODAnalysis* alg = new MyxAODAnalysis();
    job.algsAdd( alg );
    /// all configuration to the algorithm should be passed after attachment to the job (practically after this comment)
 
-   alg->outputName = "myOutput"; // give the name of the output to our algorithm
+//    alg->outputName = "myOutput"; // give the name of the output to our algorithm
    //alg->m_useHistObjectDumper = false;
-   alg-m_useMuonCalibrationAndSmearingTool = false;
+   alg->m_useMuonCalibrationAndSmearingTool = false;
 
-   // Run the job using the local/direct driver:
+   /// Run the job using the local/direct driver:
    EL::DirectDriver driver;
    driver.submit( job, submitDir );
 
