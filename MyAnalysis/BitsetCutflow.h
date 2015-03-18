0bed58821f0ceec6ffa58869f3959715267ab6e4
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-18 20:14:04 +0100

    fixes

--- MyAnalysis/BitsetCutflow.h
+++ MyAnalysis/BitsetCutflow.h
@@ -17,27 +17,27 @@ public:
 	BitsetCutflow(EL::Worker *wk);
 	~BitsetCutflow();
 
-// 	void FillCutflow(string cutflowStepName);
+	void FillCutflow(string cutflowStepName);
 	
 	/// WARNING call this function on the END of EVENT!!!
-// 	void PushBitSet();
+	void PushBitSet();
 	
 private:
 	
-// 	unsigned int m_cutflowStepCounter; //!
-// 	
-// 	/// link to EventLoop worker;
-// 	EL::Worker *m_wk; //!
-// 	
-// 	/// actual bitset were we will store cutflow on event-by-event basis
-// 	bitset<99> m_bitset; //!
-// 	
-// 	/// map with cutflow steps
-// 	map<string,unsigned int> m_mapCutflowSteps; //!
-// 	
-// 	TH1I* m_cutflowHist; //!
-// 	
-// 	/// this is needed to distribute the algorithm to the workers
+	unsigned int m_cutflowStepCounter; //!
+	
+	/// link to EventLoop worker;
+	EL::Worker *m_wk; //!
+	
+	/// actual bitset were we will store cutflow on event-by-event basis
+	bitset<99> m_bitset; //!
+	
+	/// map with cutflow steps
+	map<string,unsigned int> m_mapCutflowSteps; //!
+	
+	TH1I* m_cutflowHist; //!
+	
+	/// this is needed to distribute the algorithm to the workers
 	ClassDef(BitsetCutflow, 1);
 
 };
--- Root/BitsetCutflow.cxx
+++ Root/BitsetCutflow.cxx
@@ -4,39 +4,39 @@
 ClassImp(BitsetCutflow)
 
 BitsetCutflow::BitsetCutflow(EL::Worker *wk){
-// 	m_wk = wk;
-// 	m_cutflowHist = new TH1I("cutflow_hist","Cutflow",99,0,99);
-// 	m_wk->addOutput(m_cutflowHist);
-// 	m_cutflowStepCounter = 0;
+	m_wk = wk;
+	m_cutflowHist = new TH1I("cutflow_hist","Cutflow",99,0,99);
+	m_wk->addOutput(m_cutflowHist);
+	m_cutflowStepCounter = 0;
 }
 
 BitsetCutflow::~BitsetCutflow(){
 	
 }
 
-// void BitsetCutflow::PushBitSet(){
-// 	
-// 	/// All events
-// 	m_cutflowHist->Fill(0);
-// 	
-// 	for (unsigned i=1; i<=m_mapCutflowSteps.size(); i++){
-// 		/// first bit is always zero
-// 		if (m_bitset[i]!=m_bitset[0]) m_cutflowHist->Fill(i); 
-// 	}
-// 	
-// 	/// clean bit for next event
-// 	m_bitset.reset();
-// }
+void BitsetCutflow::PushBitSet(){
+	
+	/// All events
+	m_cutflowHist->Fill(0);
+	
+	for (unsigned i=1; i<=m_mapCutflowSteps.size(); i++){
+		/// first bit is always zero
+		if (m_bitset[i]!=m_bitset[0]) m_cutflowHist->Fill(i); 
+	}
+	
+	/// clean bit for next event
+	m_bitset.reset();
+}
 
-// void BitsetCutflow::FillCutflow(string cutflowStepName){
-// 	/// check if there is a slot already created for this cutflow step
-// 	unsigned int slotPosition = m_mapCutflowSteps[cutflowStepName];
-// 	if (slotPosition==0){
-// 		m_cutflowStepCounter++;
-// 		m_mapCutflowSteps[cutflowStepName] = m_cutflowStepCounter;
-// 		slotPosition = m_cutflowStepCounter;
-// 		m_cutflowHist->GetXaxis()->SetBinLabel(m_cutflowStepCounter,cutflowStepName.c_str());
-// 	}
-// 	m_bitset[slotPosition]=1;
-// }
+void BitsetCutflow::FillCutflow(string cutflowStepName){
+	/// check if there is a slot already created for this cutflow step
+	unsigned int slotPosition = m_mapCutflowSteps[cutflowStepName];
+	if (slotPosition==0){
+		m_cutflowStepCounter++;
+		m_mapCutflowSteps[cutflowStepName] = m_cutflowStepCounter;
+		slotPosition = m_cutflowStepCounter;
+		m_cutflowHist->GetXaxis()->SetBinLabel(m_cutflowStepCounter,cutflowStepName.c_str());
+	}
+	m_bitset[slotPosition]=1;
+}
 
