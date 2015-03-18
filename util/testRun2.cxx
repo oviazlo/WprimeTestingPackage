9f1e38acf0fc1ffa7a7eb0fe4591dde233544916
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-18 15:13:03 +0100

    fixes

--- util/testRun2.cxx
+++ util/testRun2.cxx
@@ -10,63 +10,151 @@
 #include <EventLoopAlgs/NTupleSvc.h>
 #include <EventLoop/OutputStream.h>
 
+/// boost
+#include "boost/program_options.hpp"
+#include <boost/algorithm/string.hpp>
+
+int parseOptionsWithBoost(po::variables_map &vm, int argc, char* argv[]);
+
+namespace 
+{ 
+  const size_t ERROR_IN_COMMAND_LINE = 1; 
+  const size_t SUCCESS = 0; 
+  const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
+  const size_t HELP_CALLED = 3;
+  
+  namespace po = boost::program_options;
+ 
+} /// namespace 
+
 int main( int argc, char* argv[] ) {
 
-   // Take the submit directory from the input if provided:
-   std::string submitDir = "submitDir";
-   if( argc > 1 ) submitDir = argv[ 1 ];
+	/// get global input arguments:
+	po::variables_map vm; 
+	const size_t returnedMessage = parseOptionsWithBoost(vm,argc,argv);
+	if (returnedMessage!=SUCCESS) std::exit(returnedMessage);
 
-   int nEvent = -1;
-   if( argc > 2 ) nEvent = atoi(argv[ 2 ]);
-   
-   // Set up the job for xAOD access:
-   xAOD::Init().ignore();
+	/// Take the submit directory from the input if provided:
+	std::string submitDir = "submitDir";
+	if ( vm.count("folder") ){
+		submitDir = vm["folder"].as<vector<std::string> >();  
+	}
 
-   // Construct the samples to run on:
-   SH::SampleHandler sh;
+	int nEvents = -1;
+	if ( vm.count("nEvents") ){
+		nEvents = vm["nEvents"].as<vector<unsigned int> >();  
+	}
+	
+	// Set up the job for xAOD access:
+	xAOD::Init().ignore();
 
-   const char* inputFilePath = gSystem->ExpandPathName ("/afs/cern.ch/work/o/oviazlo/Wprime/datasets");
-   //const char* inputFilePath = gSystem->ExpandPathName ("$ALRB_TutorialData/r5591"); // using $ALRB_TutorialData previously defined
-   SH::DiskListLocal list (inputFilePath);
-   SH::scanDir (sh, list, "DAOD_EXOT9*root*");
-   //SH::scanDir (sh, list, "DAOD_EXOT9.04655450._000002.pool.root.1");
-   //SH::scanDir (sh, list, "AOD.01494882._113691.pool.root.1"); // specifying one particular file for testing
+	// Construct the samples to run on:
+	SH::SampleHandler sh;
 
-   /// Set the name of the input TTree. It's always "CollectionTree"
-   /// for xAOD files.
-   sh.setMetaString( "nc_tree", "CollectionTree" );
+	const char* inputFilePath = gSystem->ExpandPathName ("/afs/cern.ch/work/o/oviazlo/Wprime/datasets");
+	//const char* inputFilePath = gSystem->ExpandPathName ("$ALRB_TutorialData/r5591"); // using $ALRB_TutorialData previously defined
+	SH::DiskListLocal list (inputFilePath);
+	SH::scanDir (sh, list, "DAOD_EXOT9*root*");
+	//SH::scanDir (sh, list, "DAOD_EXOT9.04655450._000002.pool.root.1");
+	//SH::scanDir (sh, list, "AOD.01494882._113691.pool.root.1"); // specifying one particular file for testing
 
-   /// Print what we found:
-   sh.print();
+	/// Set the name of the input TTree. It's always "CollectionTree"
+	/// for xAOD files.
+	sh.setMetaString( "nc_tree", "CollectionTree" );
 
-   /// Create an EventLoop job:
-   EL::Job job;
-   job.sampleHandler( sh );
+	/// Print what we found:
+	sh.print();
 
-   /// Specify that we only want to run on 1k events
-   if (nEvent!=-1)
-	   job.options()->setDouble(EL::Job::optMaxEvents, nEvent);
+	/// Create an EventLoop job:
+	EL::Job job;
+	job.sampleHandler( sh );
 
-   /// define an output and an ntuple associated to that output 
-//    EL::OutputStream output  ("myOutput");
-//    job.outputAdd (output);
-//    EL::NTupleSvc *ntuple = new EL::NTupleSvc ("myOutput");
-//    job.algsAdd (ntuple);
+	/// Specify that we only want to run on 1k events
+	if (nEvents!=-1)
+		job.options()->setDouble(EL::Job::optMaxEvents, nEvents);
 
-   /// Add our analysis to the job:
-   MyxAODAnalysis* alg = new MyxAODAnalysis();
-   job.algsAdd( alg );
-   /// all configuration to the algorithm should be passed after attachment to the job (practically after this comment)
+	/// define an output and an ntuple associated to that output 
+	//    EL::OutputStream output  ("myOutput");
+	//    job.outputAdd (output);
+	//    EL::NTupleSvc *ntuple = new EL::NTupleSvc ("myOutput");
+	//    job.algsAdd (ntuple);
 
-//    alg->outputName = "myOutput"; // give the name of the output to our algorithm
-   //alg->m_useHistObjectDumper = false;
-   alg->m_useMuonCalibrationAndSmearingTool = false;
+	/// Add our analysis to the job:
+	MyxAODAnalysis* alg = new MyxAODAnalysis();
+	job.algsAdd( alg );
+	/// all configuration to the algorithm should be passed after attachment to the job (practically after this comment)
 
-   // Run the job using the local/direct driver:
-   EL::ProofDriver driver;
-   driver.numWorkers = 2;
-   //EL::DirectDriver driver;
-   driver.submit( job, submitDir );
+	//    alg->outputName = "myOutput"; // give the name of the output to our algorithm
+	//alg->m_useHistObjectDumper = false;
+	alg->m_useMuonCalibrationAndSmearingTool = false;
 
-   return 0;
+	if ( vm.count("proof") ){/// Run the job using the local/direct driver:
+		EL::ProofDriver driver;
+		if ( vm.count("nWorkers") ){
+			driver.numWorkers = vm["nWorkers"].as<vector<unsigned int> >();  
+		}
+		driver.submit( job, submitDir );
+	}
+	else{/// Run the job using the local/direct driver:
+		EL::DirectDriver driver;
+		driver.submit( job, submitDir );
+	}
+	
+	return 0;
 }
+
+
+
+/// parse input arguments
+int parseOptionsWithBoost(po::variables_map &vm, int argc, char* argv[]){
+  
+  try 
+  { 
+    /** Define and parse the program options 
+     */ 
+    po::options_description desc("Options"); 
+    desc.add_options()
+      ("help,h", "Print help messages") 
+      ("folder,f", po::value<std::vector<string> >(), "output working-folder name")
+      ("nWorkers,w", po::value<unsigned int>(), "number of workers")
+      ("proof,p", "enable PROOF-Lite mode") 
+      ("nEvents,n", po::value<unsigned int>(), "number of events to proceed")
+      ;
+    try 
+    { 
+      po::store(po::parse_command_line(argc, argv, desc),  
+                vm); /// can throw 
+ 
+      /** --help option 
+       */ 
+      if ( vm.count("help")  ) 
+      { 
+        std::cout << "Basic Command Line Parameter App" << std::endl 
+                  << desc << std::endl; 
+        return HELP_CALLED; 
+      } 
+      
+      po::notify(vm); /// throws on error, so do after help in case 
+                      /// there are any problems 
+    } 
+    catch(po::error& e) 
+    { 
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
+      std::cerr << desc << std::endl; 
+      return ERROR_IN_COMMAND_LINE; 
+    } 
+ 
+    /// application code here // 
+    return SUCCESS;
+ 
+  } 
+  catch(std::exception& e) 
+  { 
+    std::cerr << "Unhandled Exception reached the top of main: " 
+              << e.what() << ", application will now exit" << std::endl; 
+    return ERROR_UNHANDLED_EXCEPTION; 
+ 
+  } 
+  
+}
+
