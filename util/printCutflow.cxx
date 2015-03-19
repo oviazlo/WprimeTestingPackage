046ea9d167a766654e2308f1ee7ea1e471905fa0
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-19 23:32:12 +0100

    fixes

--- util/printCutflow.cxx
+++ util/printCutflow.cxx
@@ -10,6 +10,8 @@
 #include <TH1F.h>
 #include <TCanvas.h>
 
+#include "TStyle.h"
+
 //~ #include "AtlasStyle.C"
 //~ #include "AtlasUtils.C"
 //~ #include "AtlasUtils.h"
@@ -34,6 +36,8 @@ namespace
 } /// namespace 
 
 int parseOptionsWithBoost(po::variables_map &vm, int argc, char* argv[]);
+TStyle* AtlasStyle();
+void SetAtlasStyle();
 
 int main( int argc, char* argv[] ) {
 
@@ -68,7 +72,7 @@ int main( int argc, char* argv[] ) {
 	}
 	
 
-	//~ SetAtlasStyle()
+	SetAtlasStyle()
 	
 	// read pt, MET, Mt histograms, make them pretty and save them
 	TH1F* h_pt = (TH1F*)mySample->readHist ("muon/stage2_allCuts/pt");
@@ -88,10 +92,17 @@ int main( int argc, char* argv[] ) {
 	h_Mt->SetTitle("");
 	
 	TCanvas *can = new TCanvas();
+	gPad->SetLogy();
+	
 	h_pt->Draw();
+	h_pt->GetXaxis()->SetUserRange(0,5000.0);
 	can->SaveAs("pT.eps");
+	
+	h_MET->GetXaxis()->SetUserRange(0,5000.0);
 	h_MET->Draw();
 	can->SaveAs("MET.eps");
+	
+	h_Mt->GetXaxis()->SetUserRange(0,5000.0);
 	h_Mt->Draw();
 	can->SaveAs("mT.eps");
 	
@@ -149,4 +160,90 @@ int parseOptionsWithBoost(po::variables_map &vm, int argc, char* argv[]){
   } 
   
 }
+
+
+
+void SetAtlasStyle()
+{
+  std::cout << "\nApplying ATLAS style settings...\n" << std::endl ;
+  TStyle* atlasStyle = AtlasStyle();
+  gROOT->SetStyle("ATLAS");
+  gROOT->ForceStyle();
+}
+
+TStyle* AtlasStyle() 
+{
+  TStyle *atlasStyle = new TStyle("ATLAS","Atlas style");
+
+  // use plain black on white colors
+  Int_t icol=0; // WHITE
+  atlasStyle->SetFrameBorderMode(icol);
+  atlasStyle->SetFrameFillColor(icol);
+  atlasStyle->SetCanvasBorderMode(icol);
+  atlasStyle->SetCanvasColor(icol);
+  atlasStyle->SetPadBorderMode(icol);
+  atlasStyle->SetPadColor(icol);
+  atlasStyle->SetStatColor(icol);
+  //atlasStyle->SetFillColor(icol); // don't use: white fill color for *all* objects
+
+  // set the paper & margin sizes
+  atlasStyle->SetPaperSize(20,26);
+
+  // set margin sizes
+  atlasStyle->SetPadTopMargin(0.08);
+  atlasStyle->SetPadRightMargin(0.05);
+  atlasStyle->SetPadBottomMargin(0.16);
+  atlasStyle->SetPadLeftMargin(0.16);
+
+  // set title offsets (for axis label)
+  atlasStyle->SetTitleXOffset(1.4);
+  atlasStyle->SetTitleYOffset(1.4);
+
+  // use large fonts
+  //Int_t font=72; // Helvetica italics
+  Int_t font=42; // Helvetica
+  Double_t tsize=0.05;
+  atlasStyle->SetTextFont(font);
+
+  atlasStyle->SetTextSize(tsize);
+  atlasStyle->SetLabelFont(font,"x");
+  atlasStyle->SetTitleFont(font,"x");
+  atlasStyle->SetLabelFont(font,"y");
+  atlasStyle->SetTitleFont(font,"y");
+  atlasStyle->SetLabelFont(font,"z");
+  atlasStyle->SetTitleFont(font,"z");
+  
+  atlasStyle->SetLabelSize(tsize,"x");
+  atlasStyle->SetTitleSize(tsize,"x");
+  atlasStyle->SetLabelSize(tsize,"y");
+  atlasStyle->SetTitleSize(tsize,"y");
+  atlasStyle->SetLabelSize(tsize,"z");
+  atlasStyle->SetTitleSize(tsize,"z");
+
+  // use bold lines and markers
+  atlasStyle->SetMarkerStyle(20);
+  atlasStyle->SetMarkerSize(1.2);
+  atlasStyle->SetHistLineWidth(2.);
+  atlasStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
+
+  // get rid of X error bars 
+  //atlasStyle->SetErrorX(0.001);
+  // get rid of error bar caps
+  atlasStyle->SetEndErrorSize(0.);
+
+  // do not display any of the standard histogram decorations
+  atlasStyle->SetOptTitle(0);
+  //atlasStyle->SetOptStat(1111);
+  atlasStyle->SetOptStat(0);
+  atlasStyle->SetOptFit(1111);
+  //atlasStyle->SetOptFit(0);
+
+  // put tick marks on top and RHS of plots
+  atlasStyle->SetPadTickX(1);
+  atlasStyle->SetPadTickY(1);
+
+  return atlasStyle;
+
+}
+
 
