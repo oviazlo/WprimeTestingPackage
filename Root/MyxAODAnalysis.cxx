7c6ba451b9d98b9128841c339dcc64d8e67253da
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-20 01:17:15 +0100

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -298,7 +298,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
 				for (int j=0; j<nDecPart; j++){
 					const xAOD::TruthParticle* decayPart = decayVtx->outgoingParticle(j);
 					double decayPartPt = TMath::Sqrt(decayPart->px()*decayPart->px() + decayPart->py()*decayPart->py())*0.001;
-					cout << j << ": " << decayPart->pdgId() << " (pT = " << decayPartPt << " GeV)";
+					cout << endl << j << ": " << decayPart->pdgId() << " (pT = " << decayPartPt << " GeV)";
 				}
 				cout << endl;
 			}
