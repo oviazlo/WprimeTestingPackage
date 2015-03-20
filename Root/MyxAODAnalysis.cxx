3743f3b03ae7ef85552bb44012fb29bb25fca6fc
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-03-20 01:31:56 +0100

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -539,6 +539,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
 		
 		
 	cout << "Event number: " << m_eventCounter << endl;
+	if (m_eventCounter==68) return EL::StatusCode::SUCCESS;
 	xAOD::TruthEventContainer::const_iterator itr;
 	for (itr = xTruthEventContainer->begin(); itr!=xTruthEventContainer->end(); ++itr) {
 		int nVert = (*itr)->nTruthVertices();
