1a23b6d0270279d018a1841c799a16a883a4d84b
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-06-01 22:01:01 +0200

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -508,7 +508,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
 			if (abs( z0_vrtPVx*sintheta )<10.0) continue;
 			m_BitsetCutflow->FillCutflow("z0");
 			
-			const xAOD::Muon *constMuon = mu;
+			const xAOD::Muon &constMuon = mu;
 			if (!m_muonSelection->passedHighPtCuts(constMuon)) continue;
 			m_BitsetCutflow->FillCutflow("MS Hits");
 			
