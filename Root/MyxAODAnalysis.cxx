2d1b264ec9f25a8d3759493afb98ea9166d4fa4f
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-06-19 10:28:34 +0200

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -260,9 +260,9 @@ EL::StatusCode MyxAODAnalysis :: initialize ()
   
   ///
   m_isolationSelectionTool = new CP::IsolationSelectionTool("iso");
-  EL_RETURN_CHECK( "initialize",m_isolationSelectionTool.setProperty(
+  EL_RETURN_CHECK( "initialize",m_isolationSelectionTool->setProperty(
     "MuonWP","VeryLooseTrackOnly") );
-  EL_RETURN_CHECK( "initialize",m_isolationSelectionTool.initialize()); 
+  EL_RETURN_CHECK( "initialize",m_isolationSelectionTool->initialize()); 
   
   if (m_useHistObjectDumper)
     m_HistObjectDumper = new HistObjectDumper(wk());
@@ -561,7 +561,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
       m_BitsetCutflow->FillCutflow("z0");
       
       /// Isolation stuff
-      if (!m_isolationSelectionTool.accept(mu)) continue;
+      if (!m_isolationSelectionTool->accept(mu)) continue;
       m_BitsetCutflow->FillCutflow("Isolation");
       
       double phi_mu = mu->phi();
@@ -625,7 +625,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
       if (abs( z0_vrtPVx*sintheta )>10.0) continue;
       
       /// Isolation stuff
-      if (!m_isolationSelectionTool.accept(mu)) continue;
+      if (!m_isolationSelectionTool->accept(mu)) continue;
       
       nVetoMuons++;
     }
