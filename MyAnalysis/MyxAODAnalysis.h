9b447778d5f71d0571a20a050e3eca2c190196ad
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-08-28 12:44:35 +0200

    fixes

--- MyAnalysis/MyxAODAnalysis.h
+++ MyAnalysis/MyxAODAnalysis.h
@@ -214,7 +214,7 @@ public:
   TH1 *h_truthMET; //!
 
   /// defining the output file name and tree that we will put in the output ntuple, also the one branch that will be in that tree 
-//   std::string outputName; //!
+  std::string outputName; //!
 //   TTree *tree; //!
 
   #ifndef __CINT__
