ce7f85ed3aaf22b90f010176bc2c4b7ee60db1a9
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-11-21 12:02:58 +0100

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -178,7 +178,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
 
 //   m_store->record(classifiedMuons.first,  "classifiedMuons");
 //   m_store->record(classifiedMuons.second, "classifiedMuonsAux");
-  
+  /*
   std::pair<unsigned int, unsigned int> muPair = SelectMuons(classifiedMuons.first, 
                                                              primVertex, true);
   
@@ -218,9 +218,9 @@ EL::StatusCode MyxAODAnalysis :: execute ()
 
 //   m_store->record(classifiedElectrons.first,  "classifiedElectrons");
 //   m_store->record(classifiedElectrons.second, "classifiedElectronsAux");
-  
+  */
   std::pair<unsigned int, unsigned int> elPair = 
-  SelectElectrons( classifiedElectrons.first, false );
+  SelectElectrons( classifiedElectrons.first, true );
   
   if (elPair.first!=0 || elPair.second!=0)
     return EL::StatusCode::SUCCESS;
