6bed7f133a99110ccdba7b6311d4e56ae705203b
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-11-23 03:57:32 +0100

    fixes

--- util/runMainLoop.cxx
+++ util/runMainLoop.cxx
@@ -66,14 +66,24 @@ int main( int argc, char* argv[] ) {
   /// define which input-path to use
   /// look for HOSTNAME env. variable
   std::size_t found = hostName.find("cern");
-  if (found!=std::string::npos)
+  if (found!=std::string::npos){
     /// cern machines
     inputFilePath = gSystem->ExpandPathName
     ("/afs/cern.ch/work/o/oviazlo/Wprime/AnalysisFramework/rel20/data");
-  else
-    /// lunarc machines
-    inputFilePath = gSystem->ExpandPathName
-    ("/nfs/shared/pp/oviazlo/xAOD/p2425");
+  }
+  else{
+    std::size_t found = hostName.find("alarik");
+    if (found!=std::string::npos){
+      /// alarik cluster machines
+      inputFilePath = gSystem->ExpandPathName
+      ("/lunarc/nobackup/users/oviazlo/xAOD/p2425");
+    }
+    else{
+      /// iridium cluster
+      inputFilePath = gSystem->ExpandPathName
+      ("/nfs/shared/pp/oviazlo/xAOD/p2425"); 
+    }
+  }
 
   SH::DiskListLocal list (inputFilePath);
   SH::scanDir (sh, list, "DAOD_EXOT9.*root*");
