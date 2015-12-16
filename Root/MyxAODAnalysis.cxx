2753716e0dca2b8edf61d09d15e077c28a4de7ef
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2015-12-16 21:31:27 +0100

    fixes

--- Root/MyxAODAnalysis.cxx
+++ Root/MyxAODAnalysis.cxx
@@ -427,6 +427,7 @@ EL::StatusCode MyxAODAnalysis :: execute ()
   m_BitsetCutflow->FillCutflow("mT");
   
   /// get MC weights
+  m_LPXKfactorTool->execute();
   weightkFactor = eventInfo->auxdecor<double>("KfactorWeight");
   weighfilterEfficiency = m_LPXKfactorTool->getMCFilterEfficiency();
   weightCrossSection = 1.0/m_LPXKfactorTool->getMCCrossSection(); ///TODO make proper implementation
