d949e9cc93b7f1cb80d926e3158064700f4257a2
Oleksandr Viazlo <oleksandr.viazlo@cern.ch>
2016-01-07 16:35:56 +0100

    add util/HelpFunctions.h

---
+++ util/HelpFunctions.h
@@ -0,0 +1,12 @@
+#ifndef HelpFunctions_H
+#define HelpFunctions_H
+
+#include "TStyle.h"
+#include "TROOT.h"
+#include "iostream.h"
+
+
+int parseOptionsWithBoost(po::variables_map &vm, po::options_description desc, 
+                          int argc, char* argv[]);
+TStyle* AtlasStyle();
+void SetAtlasStyle();
